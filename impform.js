8b752fbdaed32d69ce1f62bcea2252a6e58bf081
Andrey <79952000200@ya.ru>
2020-03-30 18:46:37 +0300

    test1

--- impform.js
+++ impform.js
@@ -2,6 +2,6 @@ import square from './form.js';
 import sumOfSquares from './form.js';
 import squareSumOfSquares from './form.js';
 
-console.log(mate.square(2);
-console.log(mate.sumOfSquares(2, 3));
-console.log(mate.squareSumOfSquares(3, 4));
+console.log(square(2);
+console.log(sumOfSquares(2, 3));
+console.log(squareSumOfSquares(3, 4));
